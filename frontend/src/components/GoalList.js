e7a175ad6bff72b79fc99891efc3a0458352fab7
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-28 12:57:02 +0530

    Goal comp created

---
+++ frontend/src/components/GoalList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './GoalList.css';
+
+const GoalList = () => {
+  return (
+    <ul className="goal-list">
+      <li>Finish th course</li>
+      <li>Learn all about the course Main Topic</li>
+      <li>Help other students in the course Q&A</li>
+    </ul>
+  );
+};
+
+export default GoalList;
