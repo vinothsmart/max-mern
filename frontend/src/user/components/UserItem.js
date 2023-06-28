3f5c536387366e86ce72404e036d646738aa28c1
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-28 17:19:47 +0530

    listed user

--- frontend/src/user/components/UserItem.js
+++ frontend/src/user/components/UserItem.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './UserItem.css';
 
-const UserItem = ({ id, image }) => {
-  return <div>UserItem</div>;
+const UserItem = ({ id, name, image, places }) => {
+  return (
+    <li key={id} className="user-item">
+      <div className="user-item__content">
+        <div className="user-item__image">
+          <img src={image} alt="user" />
+        </div>
+        <div className="user-item__info">
+          <h2>{name}</h2>
+          <h3>
+            {places} {places.lenght === 1 ? 'Place' : 'Places'}
+          </h3>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default UserItem;
