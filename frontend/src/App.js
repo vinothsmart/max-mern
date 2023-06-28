bd7d795ecc5ad474a0b5719b1143c194a477b1fc
vinothsmart <s.vinothkanna90@gmail.com>
2023-06-28 17:01:04 +0530

    router and redirect

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <h1>Hi Vinoth</h1>;
-}
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+  Navigate,
+} from 'react-router-dom';
+import NewPlace from './places/pages/NewPlace';
+import Users from './user/pages/Users';
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Users />} />
+        <Route path="places">
+          <Route path="new" element={<NewPlace />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    )
+  );
+  return <RouterProvider router={router} />;
+};
 
 export default App;
