b1e348b8df7c8f1b1c97aaf30cc7e2ed93ec41bc
vinothsmart <s.vinothkanna90@gmail.com>
2023-07-24 16:47:40 +0530

    common Hoc For page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,16 +5,32 @@ import {
   RouterProvider,
   Navigate,
 } from 'react-router-dom';
+import MainNavigation from './places/components/Navigation/MainNavigation';
 import NewPlace from './places/pages/NewPlace';
 import Users from './user/pages/Users';
+
+// create HOC for MainNavigation
+const withMainNavigation = (Component) => (props) =>
+  (
+    <>
+      <MainNavigation />
+      <main>
+        <Component {...props} />
+      </main>
+    </>
+  );
+
+// wrap Users and NewPlace with MainNavigation
+const UsersLayout = withMainNavigation(Users);
+const NewPlaceLayout = withMainNavigation(NewPlace);
 
 const App = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/">
-        <Route index element={<Users />} />
+        <Route index element={<UsersLayout />} />
         <Route path="places">
-          <Route path="new" element={<NewPlace />} />
+          <Route path="new" element={<NewPlaceLayout />} />
         </Route>
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
