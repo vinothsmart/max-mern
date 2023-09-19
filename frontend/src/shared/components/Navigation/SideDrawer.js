eb107220a9e7be9f2df7bc9da68cd9d5312bdf9c
vinothsmart <s.vinothkanna90@gmail.com>
2023-09-19 16:21:03 +0530

    createPortal

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { createPortal } from "react-dom";
 
 import "./SideDrawer.css";
 
 const SideDrawer = ({ children }) => {
-  return <aside className="side-drawer">{children}</aside>;
+  const content = <aside className="side-drawer">{children}</aside>;
+  return createPortal(content, document.getElementById("drawer-hook"));
 };
 
 export default SideDrawer;
