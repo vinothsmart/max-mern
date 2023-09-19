0dbdbbe001d73bd47d478b79afbf231e6b6b6b45
vinothsmart <s.vinothkanna90@gmail.com>
2023-09-19 16:09:45 +0530

    sidebar added

--- frontend/src/shared/components/Navigation/MainNavigation.js
+++ frontend/src/shared/components/Navigation/MainNavigation.js
@@ -2,22 +2,30 @@ import { Link } from "react-router-dom";
 import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 const MainNavigation = () => {
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Places</Link>
-      </h1>
-      <nav className="main-navigation__header-nav">
-        <NavLinks />
-      </nav>
-    </MainHeader>
+    <>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Places</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </>
   );
 };
 
---
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./SideDrawer.css";
+
+const SideDrawer = ({ children }) => {
+  return <aside className="side-drawer">{children}</aside>;
+};
+
+export default SideDrawer;
